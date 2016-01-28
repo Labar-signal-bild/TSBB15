24f9104482bafdfabc35f147d8723f928108d5b0
frefr166 <frefr166@student.liu.se>
2016-01-28 19:20:31 +0100

    LKgrad and LKtensor done.

--- Lab1/LkGrad.m
+++ Lab1/LkGrad.m
@@ -1,6 +1,9 @@
 function [ fx fy] = LkGrad(Im,N,sigma)
 %LKGRAD Estimate the regularizeed gradient for an image
-
+if sigma == 0
+    sigma = 0.1;
+    disp('----- 0 std into LkGrad -----');
+end
 lp=exp(-0.5*([-N:N]/sigma).^2);
 lp=lp/sum(lp);
 df=-1/sigma^2*[-N:N].*lp;
--- Lab1/LkTensor.m
+++ Lab1/LkTensor.m
@@ -1,7 +1,21 @@
 function [ T ] = LkTensor( Im_reg )
 %LKTENSOR Estimate a tensor for a specific region
+%[ fx fy] = LkGrad(Im,filter_size,Im_std)
 
-
-
+lengthy = 1:size(Im_reg,1);
+lengthx= 1:size(Im_reg,2);
+Im_std = mean(std(double(Im_reg)));
+%Decide filter size depending on image size (place in triangle)
+filter_size = ceil(length(Im_reg)/1000);
+[fx fy] = LkGrad (Im_reg,filter_size,Im_std);
+T  = zeros(2,2);
+for y = lengthy
+    for x = lengthx
+        T = T+[ fx(x,y).^2, fx(x,y) .* fy(x,y)  ; ...
+                fx(x,y) .* fy(x,y)  ,fy(x,y).^2   ]; 
+    end
+    
+end
+1
 end
 
