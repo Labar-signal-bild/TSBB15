db9bf1dd2ff4c111e8da86b610e6d8955152f710
Alexander Poole <alepo020@student.liu.se>
2016-01-28 18:28:08 +0100

    Added LkGrad

--- Lab1/LkGrad.m
+++ Lab1/LkGrad.m
@@ -1,6 +1,11 @@
-function [ Grad_Im] = LkGrad(Im,filter_size,Im_std)
+function [ fx fy] = LkGrad(Im,N,sigma)
 %LKGRAD Estimate the regularizeed gradient for an image
 
+lp=exp(-0.5*([-N:N]/sigma).^2);
+lp=lp/sum(lp);
+df=-1/sigma^2*[-N:N].*lp;
+fx=conv2(lp,df',Im,'same');
+fy=conv2(df,lp',Im,'same');
 
 end
 
