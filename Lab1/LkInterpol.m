9b36ed050fc050dfb6466613a5d28c7985c7c067
frefr166 <frefr166@student.liu.se>
2016-01-29 17:06:33 +0100

    Interpolation started

--- Lab1/LkInterpol.m
+++ Lab1/LkInterpol.m
@@ -1,7 +1,18 @@
-function [ output_args ] = LkInterpol(Im)
-%LKINTERPOL Don't know how this will work jet
+function [ Int_Im ] = LkInterpol(Im,du,method)
+%LKINTERPOL Returns value from interpolation interval
+Imy = size(Im,1);
+Imx = size(Im,2);
+upsample_size = 1/du;
+Int_Im = zeros(size(Im));
+Int = interp2(Im,upsample_size)
 
-
-
+for yit = 1:Imy
+    for xit = 1:Imx
+        
+        Int_Im(yit,xit) = Int(xit*upsample_size,yit*upsample_size)
+        
+        %Vilket värde ska vi hämta ut när vi läser mellan...?
+    end
 end
 
+end
