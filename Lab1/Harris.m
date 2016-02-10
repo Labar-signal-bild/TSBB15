0c4aa470a19482608accc1fc0400441276642a01
Alexander Poole <alepo020@student.liu.se>
2016-02-10 19:20:52 +0100

    Lab 1 done!

--- Lab1/Harris.m
+++ Lab1/Harris.m
@@ -6,90 +6,55 @@ function [ Im_intrest ] = Harris( Im , K)
 
 k = 0.1;
 cHarris = (T11.*T22-T12.^2) - k*(T11+T22).^2; % The Harris response in each pixel
-%figure(2)
-%maxv2 = max(max(abs(cHarris)))/2;
-%imagesc(cHarris, [-maxv2 maxv2]); colorbar('horizontal'); 
-%axis image; axis off;
-%title('cHarris')
-%size(cHarris)
+
 
 
 histo = hist(cHarris);
-stem(histo);
-
 histVec = sum(histo,2);
 tresh = find(histVec == max(histVec));
 tresh = (tresh-0.5)*sum(histVec);
-
+%Decide the threshold by taking max of histogram.
 
 cHarrisNew = zeros(size(cHarris,1), size(cHarris,2));
 treshPoints = find(cHarris > tresh);
 cHarrisNew(treshPoints) = 1;
-
+%Create a mask with all harris points larger than thresh.
 cHarrisValues = cHarrisNew.*cHarris;
-
-maxv2 = max(max(abs(cHarrisNew)))/2;
-%colormap(gray(256))
-%figure(1)
-%imagesc(cHarrisNew, [0 maxv2]); colorbar('horizontal'); 
-%axis image; axis off;
-%title('cHarrisNew')
-
+%Extract all harris values  with points larger than thresh.
 
 RegMax = imregionalmax(cHarris, 4);
-%figure(23)
-%colormap(gray(256))
-%imagesc(RegMax, [0 maxv2]); colorbar('horizontal'); 
-%axis image; axis off;
-%title('regmax')
-
-%figure(1338)
-%maxv2 = max(max(abs(cHarrisNew)))/2;
-%imagesc(cHarrisNew, [-maxv2 maxv2]); colorbar('horizontal'); 
-%axis image; axis off;
-%title('cHarrisnew')
+%Max value - binary!
 
 HarrisPoints = cHarrisNew.*RegMax;
+%Mask with max values that are also harris points
 cHarrisValues = cHarrisValues.*HarrisPoints;
+%Extract all max harris values
 
 Imm = ordfilt2(cHarrisValues,9,ones(3)); 
+Imm(Imm == 0) = NaN;
 [row,col] = find(cHarrisValues==Imm);
-%cHarrisValuesdilated = cHarrisValues(row,col);
+%All non-zero values should be kept. Zero is not interesting -> NaN.
+
+cHarrisValuesdilated = zeros(1,length(col));
+for i = 1:length(col)
+cHarrisValuesdilated(i) = cHarrisValues(row(i),col(i));  
+end
+[Maxval,I] = sort(cHarrisValuesdilated);
+
+rowz = row(I(end-K-1:end))
+colz = col(I(end-K-1:end))
+Im_intrest = [rowz colz]
+
+%Extract all max harris values that are far away from eachother.
+
+r = ones(length(colz), 1)*3;
 
 
-%figure(1339)
-
-%imagesc(HarrisPoints, [0 1]); colorbar('horizontal'); 
-%axis image; axis off;
-%title('harris points')
-%hpsum = sum(sum(sum(HarrisPoints)))
-
-%HarrisPoints = bwmorph(HarrisPoints,'dilate',5);
-%HarrisPoints = bwmorph(HarrisPoints,'shrink',5);
+figure(30); clf; imagesc(Im); colormap(gray(256))
+figure(30);hold('on'); plot(colz,rowz, 'go')
+viscircles([colz rowz], r ,'EdgeColor', 'r');
 
 
-cornerpoints = find(HarrisPoints == 1); %Ger en vector med pixlar på alla vita corners
-[x y] = ind2sub(size(Im),cornerpoints);
-
-
-
-r = ones(length(x), 1)*3;
-
-
-%figure(22)
-%colormap(gray(256))
-%imagesc(HarrisPoints, [0 1]); colorbar('horizontal');
-%hold on;
-%viscircles([cornerpointx cornerpointy], r ,'EdgeColor', 'r');
-%axis image; axis off;
-%title('HarrisPoints')
-
-figure(30); imagesc(Im); colormap(gray(256))
-figure(30);hold('on'); plot(y,x, 'go')
-%viscircles([y x], r ,'EdgeColor', 'r');
-
-
-Im_intrest = 1;
 
 
 end
--- Lab1/Lab1.m
+++ Lab1/Lab1.m
@@ -4,6 +4,24 @@ disp('------ Lab 1 LK-Tracker ------');
 initcourse('TSBB15');
 %% Main program
 
+Im1 = double(imread('chessboard_1.png'));
+HarrisPoints = Harris(Im1, 5);
+
+figure(11); imagesc(Im1); colormap(gray(256))
+figure(11); hold on ;plot(HarrisPoints(1:end,2), HarrisPoints(1:end,1), 'go'); hold off
+
+for i = 1:9
+    Im1 = double(imread(strcat('chessboard_',num2str(i),'.png')));
+    Im2 = double(imread(strcat('chessboard_',num2str(i+1),'.png')));   
+    [dtot e z] = LkTracker(Im1,Im2,HarrisPoints);
+    HarrisPoints = HarrisPoints + [dtot(:,2) dtot(:,1)];
+    figure(i); imagesc(Im2); colormap(gray(256));
+    figure(i);  hold on; plot(HarrisPoints(1:end,2), HarrisPoints(1:end,1), 'go'); hold off;
+    k = waitforbuttonpress 
+    
+end
+
+%% Using scale pyramids - untested below
 scale_levels = 1; %iterative variable
 smallest_scale = 10; %pixels of smallest image in scale pyramid 
 intrest_point = [85;120];
@@ -23,8 +41,7 @@ imagesc(Im2);colormap(gray);
 ScalePyramid_Im1{1} = Im1;
 ScalePyramid_Im2{1} = Im2;
 
-
-%% Scale pyramid
+% Scale pyramid - unnecessary
 while size(ScalePyramid_Im1{scale_levels},1) > smallest_scale
    scale_levels = scale_levels+1;
    ScalePyramid_Im1{scale_levels} = imresize(ScalePyramid_Im1{scale_levels-1}, 0.5);
@@ -38,35 +55,19 @@ imagesc(ScalePyramid_Im1{j});colormap(gray);
 subplot(2,scale_levels,j+scale_levels);
 imagesc(ScalePyramid_Im2{j});colormap(gray);
 end
-%% Establishing region of intrest with cHarris
+ %Establishing region of intrest with cHarris
 
 HarrisPoints = Harris(ScalePyramid_Im2{1});
 
-%% LkTracking
+% LkTracking
 % Upsamle and recompute LkTracker untill original size achived
 
 [dtot e z] = LkTracker(ScalePyramid_Im2{1},ScalePyramid_Im1{1},intrest_point);
-%%
-%d = 0;
-%for i = scale_levels:-1:1 %Start from the smallest pyramid
-% Use LkTracker to compute optimal displacement
-%d = LkTracker(ScalePyramid_Im1{i},ScalePyramid_Im2{i},d);
-%end
-
-%% Test on chessboard
-
-Im1 = double(imread('chessboard_1.png'));
-HarrisPoints = Harris(Im1, 6);
-
-figure(11); imagesc(Im1); colormap(gray(256))
-figure(11); hold on ;plot(HarrisPoints(1:end,2), HarrisPoints(1:end,1), 'go'); hold off
-
-for i = 1:9
-    Im1 = double(imread(strcat('chessboard_',num2str(i),'.png')));
-    Im2 = double(imread(strcat('chessboard_',num2str(i+1),'.png')));   
-    [dtot e z] = LkTracker(Im1,Im2,HarrisPoints);
-    HarrisPoints = HarrisPoints + [dtot(:,2) dtot(:,1)];
-    figure(i); imagesc(Im2); colormap(gray(256));
-    figure(i);  hold on; plot(HarrisPoints(1:end,2), HarrisPoints(1:end,1), 'go'); hold off;
+%
+d = 0;
+for i = scale_levels:-1:1 %Start from the smallest pyramid
+ Use LkTracker to compute optimal displacement
+d = LkTracker(ScalePyramid_Im1{i},ScalePyramid_Im2{i},d);
 end
+
 
