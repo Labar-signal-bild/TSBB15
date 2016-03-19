f84813b76f0b3855a51c2eb3d74e82e00807fd59
Fredrik Fridborn <fredrikfridborn@c-206-79.eduroam.liu.se>
2016-03-19 13:41:33 +0100

    slight modification on lab1, old exams

--- Lab1/LkTracker.m
+++ Lab1/LkTracker.m
@@ -2,8 +2,8 @@ function [ dtot,e,Z ] = LkTracker( Im1,Im2, interest_point)
 %LKTRACKER Main function for LKTracker
 dtot = zeros(size(interest_point));
 
-reg_size_x = 40/2;
-reg_size_y = 40/2;
+reg_size_x = 30/2;
+reg_size_y = 30/2;
 filter_size = 8;
 std_grad = 2;
 
@@ -11,14 +11,14 @@ grad_param = [filter_size;std_grad];
 Im2save = Im2;
 
 for j = 1:length(interest_point)
-Im1_reg = Im1(interest_point(j,1)-reg_size_x:interest_point(j,1)+reg_size_x-1,...
-             interest_point(j,2)-reg_size_y:interest_point(j,2)+reg_size_y-1);
+Im1_reg = Im1(interest_point(j,1)-reg_size_x:interest_point(j,1)+reg_size_x,...
+             interest_point(j,2)-reg_size_y:interest_point(j,2)+reg_size_y);
 
-
+4
          for i =1:10
              
-             Im2_reg = Im2(interest_point(j,1)-reg_size_x:interest_point(j,1)+reg_size_x-1,...
-                 interest_point(j,2)-reg_size_y:interest_point(j,2)+reg_size_y-1);
+             Im2_reg = Im2(interest_point(j,1)-reg_size_x:interest_point(j,1)+reg_size_x,...
+                 interest_point(j,2)-reg_size_y:interest_point(j,2)+reg_size_y);
              
              Z = LkTensor(Im2_reg,grad_param);
              e = LkDiff(Im1_reg,Im2_reg,grad_param);
